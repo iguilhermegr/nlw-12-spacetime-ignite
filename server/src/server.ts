cea82d98e7ca3184cc68a40433620e2cb06857f9
guidsribeiro <its.gr6603@gmail.com>
2023-05-21 13:28:57 -0300

    Fazendo a Aula 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,22 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
 
 app.register(cors, {
   origin: true,
 })
 
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
